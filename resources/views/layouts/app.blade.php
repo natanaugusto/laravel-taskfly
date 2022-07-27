be0251bde767ffd35489cf551c4955284349b907
Natan Augusto <natanaugusto@gmail.com>
2022-07-27 11:50:28 -0300

    Added Laravel Livewire Tables to the project
    ```shell
    sail composer require rappasoft/laravel-livewire-tables --dev
    ```
    [Laravel Livewire Tables Documentation](https://rappasoft.com/docs/laravel-livewire-tables/v2/introduction)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,9 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         @livewireStyles
+        <style>
+            [x-cloak] { display: none !important; }
+        </style>
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/rappasoft/laravel-livewire-tables/resources/views/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
