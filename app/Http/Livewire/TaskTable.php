25821d2cd4a499828e185fe0fca124e2e32178ce
Natan Augusto <natanaugusto@gmail.com>
2022-07-27 12:54:23 -0300

    Switched custom TableComponent to Livewire Tables(package) (The files from #dbf6a9700dd22e992fb61e1fb9d0729f7ec8dc6e)
    ```shell
    sail npm run dev
    ```

---
+++ app/Http/Livewire/TaskTable.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use App\Models\Task;
+
+class TaskTable extends DataTableComponent
+{
+    protected $model = Task::class;
+
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make("Id", "id")
+                ->sortable(),
+            Column::make("Creator id", "creator_id")
+                ->sortable(),
+            Column::make("Title", "title")
+                ->sortable(),
+            Column::make("Due", "due")
+                ->sortable(),
+            Column::make("Created at", "created_at")
+                ->sortable(),
+            Column::make("Updated at", "updated_at")
+                ->sortable(),
+        ];
+    }
+}
