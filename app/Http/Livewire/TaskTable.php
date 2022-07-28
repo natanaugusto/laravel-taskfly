5d53d6c362d1661eb39febfd44e6fc3ab2a5ae50
Natan Augusto <natanaugusto@gmail.com>
2022-07-28 13:13:12 -0300

    Playing with Livewire Table (And some styles changes)
    - Named parameters
    - Added Creator name on Table
    - Added collapseOnMobile
    - Disabled Select Columns and Search on table

--- app/Http/Livewire/TaskTable.php
+++ app/Http/Livewire/TaskTable.php
@@ -12,24 +12,37 @@ class TaskTable extends DataTableComponent
 
     public function configure(): void
     {
-        $this->setPrimaryKey('id');
+        $this->setPrimaryKey(key: 'id');
+        $this->setTableWrapperAttributes([
+            'default' => false,
+            'class' => 'shadow border-b border-gray-200 dark:border-gray-700 sm:rounded-lg'
+        ]);
+        $this->setSearchDisabled();
+        $this->setColumnSelectDisabled();
     }
 
     public function columns(): array
     {
         return [
-            Column::make("Id", "id")
+            Column::make(title: __(key: 'Id'), from: 'id')
+                ->sortable()
+                ->collapseOnMobile(),
+            Column::make(title: __(key: 'Creator'), from: 'creator.name')
+                ->sortable()
+                ->collapseOnMobile(),
+            Column::make(title: __(key: 'Title'), from: 'title')
                 ->sortable(),
-            Column::make("Creator id", "creator_id")
+            Column::make(title: __(key: 'Due'), from: 'due')
+                ->sortable()
+                ->collapseOnMobile(),
+            Column::make(title: __(key: 'Status'), from: 'status')
                 ->sortable(),
-            Column::make("Title", "title")
-                ->sortable(),
-            Column::make("Due", "due")
-                ->sortable(),
-            Column::make("Created at", "created_at")
-                ->sortable(),
-            Column::make("Updated at", "updated_at")
-                ->sortable(),
+            Column::make(title: __(key: 'Created at'), from: 'created_at')
+                ->sortable()
+                ->collapseOnMobile(),
+            Column::make(title: __(key: 'Updated at'), from: 'updated_at')
+                ->sortable()
+                ->collapseOnMobile(),
         ];
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div class="max-w-6xl mx-auto px-2 py-3 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
             </header>
