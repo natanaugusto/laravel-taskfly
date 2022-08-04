879bbdf406e8f8b40acb3701bfe44e6fe63222e5
Natan Augusto <natanaugusto@gmail.com>
2022-08-04 13:56:58 -0300

    Added action buttons (But just for visual proposes)

--- tests/Feature/Livewire/TaskTableTest.php
+++ tests/Feature/Livewire/TaskTableTest.php
@@ -3,15 +3,22 @@
 use App\Http\Livewire\TaskTable;
 use Livewire\Livewire;
 
-it(description: 'Mount Livewire TaskTable', closure: function () {
-    $component = Livewire::test(name: TaskTable::class);
+it(description:'Mount Livewire TaskTable', closure:function () {
+    $component = Livewire::test(name:TaskTable::class);
     $component->assertHasNoErrors();
     $instance = $component->instance();
-    expect(value: $instance)->toBeInstanceOf(class: TaskTable::class);
-    expect(value: $instance->getPrimaryKey())
-        ->toBe(expected: TaskTable::PRIMARY_KEY);
-    expect(value: $instance->getTableWrapperAttributes())
-        ->toBe(expected: TaskTable::TABLE_WRAPPER_ATTRS);
-    expect(value: $instance->getSearchStatus())->toBeFalse();
-    expect(value: $instance->getColumnSelectStatus())->toBeFalse();
+    expect(value:$instance)->toBeInstanceOf(class :TaskTable::class);
+    expect(value:$instance->getPrimaryKey())
+        ->toBe(expected:TaskTable::PRIMARY_KEY);
+
+    $columns = $instance->columns();
+    foreach ($columns as $value)
+    {
+        expect(value:$instance->getThAttributes(column:$value))
+            ->toBe(expected:TaskTable::TABLE_TH_ATTRS);
+    }
+    expect(value:$instance->getTableWrapperAttributes())
+        ->toBe(expected:TaskTable::TABLE_WRAPPER_ATTRS);
+    expect(value:$instance->getSearchStatus())->toBeFalse();
+    expect(value:$instance->getColumnSelectStatus())->toBeFalse();
 });
