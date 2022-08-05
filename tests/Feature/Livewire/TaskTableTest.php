cf40ead9cddb387570007cbd7328b66783cca89a
Natan Augusto <natanaugusto@gmail.com>
2022-08-05 12:25:08 -0300

    Added TaskIndex view

--- tests/Feature/Livewire/TaskTableTest.php
+++ tests/Feature/Livewire/TaskTableTest.php
@@ -1,17 +1,17 @@
 <?php
-
 use App\Http\Livewire\TaskTable;
-use Livewire\Livewire;
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
 
-it(description:'Mount Livewire TaskTable', closure:function () {
-    $component = Livewire::test(name:TaskTable::class);
-    $component->assertHasNoErrors();
-    $instance = $component->instance();
+it(description:'test Livewire\TaskTable mount', closure:function () {
+    extract(createLivewireComponentInstance(name:TaskTable::class));
     expect(value:$instance)->toBeInstanceOf(class :TaskTable::class);
     expect(value:$instance->getPrimaryKey())
         ->toBe(expected:TaskTable::PRIMARY_KEY);
 
     $columns = $instance->columns();
+    expect(value:$columns)->toBeArray();
     foreach ($columns as $value)
     {
         expect(value:$instance->getThAttributes(column:$value))
@@ -21,4 +21,17 @@ it(description:'Mount Livewire TaskTable', closure:function () {
         ->toBe(expected:TaskTable::TABLE_WRAPPER_ATTRS);
     expect(value:$instance->getSearchStatus())->toBeFalse();
     expect(value:$instance->getColumnSelectStatus())->toBeFalse();
+});
+
+it(description:'test Livewire\TaskTable', closure:function () {
+    /**
+     * @var Collection|User
+     */
+    $user = User::factory()->create();
+    $response = actingAs($user)->get(route(name:'tasks.index'));
+    $response->assertViewIs(value:'tasks.index');
+    $response->assertSee(__(key:'Tasks'));
+    $response->assertSeeLivewire('task-table');
+
+    extract(createLivewireComponentInstance(name:TaskTable::class));
 });
