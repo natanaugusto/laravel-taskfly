e5481fd1917436b068bbfb401a7edab6a0d4ba35
Natan Augusto <natanaugusto@gmail.com>
2022-08-07 14:12:33 -0300

    Added functional delete Button

--- resources/views/components/action-buttons.blade.php
+++ resources/views/components/action-buttons.blade.php
@@ -3,7 +3,7 @@
         class="inline-block rounded border border-gray-300 bg-yellow-300 p-1 text-xs text-gray-800 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-offset-2">
         <x-icon name="pencil-alt" />
     </a>
-    <a
+    <a href="#" wire:click.prevent="delete({{ $row->id }})"
         class="inline-block rounded border border-gray-300 bg-red-300 p-1 text-xs text-gray-800 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-offset-2">
         <x-icon name="trash" />
     </a>
