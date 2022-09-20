b6bb226c8b458cf1814ccc7274b012391c378dd9
Natan Augusto <natanaugusto@gmail.com>
2022-09-20 20:44:38 -0300

    Improve TaskTest texts (And added the trait HasEvents on Models\Task)
    - https://laravel.com/docs/9.x/eloquent#events

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasEvents;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -35,6 +36,7 @@ use Ramsey\Uuid\Uuid;
 class Task extends Model
 {
     use HasFactory;
+    use HasEvents;
     use SoftDeletes;
 
     public const DUE_DATETIME_FORMAT = 'Y-m-d H:i:s';
